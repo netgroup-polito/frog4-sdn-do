3fc4d22e70f7082cfd79dde088460173df2477e0
giacomoratta <giacomoweb@gmail.com>
2015-11-25 17:07:40 +0100

    update

--- tests/clean_db_sessions.py
+++ tests/clean_db_sessions.py
@@ -3,9 +3,9 @@ Created on Oct 23, 2015
 
 @author: fabiomignini
 '''
-from sdsa.sql.sql_server import get_session
-from sdsa.sql.graph import ActionModel, EndpointModel, EndpointResourceModel, FlowRuleModel, GraphModel, MatchModel, PortModel, GraphConnectionModel 
-from sdsa.sql.session import SessionModel
+from odl_ca_core.sql.sql_server import get_session
+from odl_ca_core.sql.graph import ActionModel, EndpointModel, EndpointResourceModel, FlowRuleModel, GraphModel, MatchModel, PortModel, GraphConnectionModel 
+from odl_ca_core.sql.session import SessionModel
 
 session = get_session()
 session.query(ActionModel).delete()
