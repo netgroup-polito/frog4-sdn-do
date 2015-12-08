48a7640f7c4a89a6b0e45be73e148c875ce04f0c
giacomoratta <giacomoweb@gmail.com>
2015-12-08 16:11:38 +0100

    update

--- main2.py
+++ main2.py
@@ -35,55 +35,8 @@ app.add_route('/NF-FG', rest_interface)
 app.add_route('/NF-FG/{nffg_id}', rest_interface)
 app.add_route('/NF-FG/status/{nffg_id}', nffg_status)
 
-
 logging.info("Falcon Successfully started")
 
-
-
-
-
-''''
-#upper_layer_API = UpperLayerOrchestrator(conf.AUTH_SERVER,conf.ORCH_USERNAME,conf.ORCH_PASSWORD,conf.ORCH_TENANT)
-#template = TemplateAPI(conf.AUTH_SERVER,conf.ORCH_USERNAME,conf.ORCH_PASSWORD,conf.ORCH_TENANT)
-#yang = YANGAPI(conf.AUTH_SERVER,conf.ORCH_USERNAME,conf.ORCH_PASSWORD,conf.ORCH_TENANT)
-
-################################################################
-
-#upper_layer_API = UpperLayerOrchestrator()
-
-
-#in_file = open("/home/giacomo/eclipse_workspace/frog-orchestrator/graphs/odlCA_graphHe_Hydrogen.json","r")
-#in_file = open("/home/giacomo/eclipse_workspace/frog4-ODL-CA/graphs/odlCA_graphHe_Lithium.json","r")
-#in_file = open("/home/giacomo/eclipse_workspace/frog4-ODL-CA/graphs/odlCA_rm1.json","r")
-in_file = open("/home/giacomo/eclipse_workspace/frog4-ODL-CA/tests/graphs/odlCA_rm1.json","r")
-
-# NF-FG File
-nffg_file = json.loads(in_file.read())
-ValidateNF_FG().validate(nffg_file)
-nffg = NF_FG()
-nffg.parseDict(nffg_file)
-
-
-# CONTROLLER
-user = UserAuthentication().authenticateUserFromCredentials("demo_jolnet", "stack", "demo_jolnet")     
-odlCA = OpenDayLightCA(user)
-
-odlCA.NFFG_Delete(977)
-
-odlCA.NFFG_Validate(nffg)
-odlCA.NFFG_Put(nffg)
-
-print("\n\nUpdating...")
-in_file = open("/home/giacomo/eclipse_workspace/frog4-ODL-CA/tests/graphs/odlCA_rm2.json","r")
-nffg_file = json.loads(in_file.read())
-ValidateNF_FG().validate(nffg_file)
-nffg = NF_FG()
-nffg.parseDict(nffg_file)
-
-odlCA.NFFG_Validate(nffg)
-odlCA.NFFG_Put(nffg)
-print("\n\nEnd update")
-'''
 
 '''
  
