d092c034706e28b719aa472168a631a29fcf2f70
giacomoratta <giacomoweb@gmail.com>
2015-12-28 20:11:14 +0100

    create database script

---
+++ create_database.py
@@ -0,0 +1,48 @@
+import sqlite3, os
+from odl_ca_core.config import Configuration
+
+def session_create_database():
+
+    db_filename = Configuration().DATABASE_CONNECTION
+    
+    # Remove first 10 characters "sqlite:///"
+    db_filename = db_filename[10:]
+    
+    # Dump file
+    db_dumpfile = "db.dump.sql"
+
+    if os.path.exists(db_dumpfile) == False:
+        print("'"+db_dumpfile+"' not exists!")
+        return
+    
+    print("Set write permissions on the database file")
+    new_db_filename = open(db_filename,'w')
+    os.chmod(db_filename, 0o666)
+    new_db_filename.close()
+    
+    print("Read the dump file")
+    in_file = open(db_dumpfile,"r")
+    sqldump = in_file.read()
+    if len(sqldump)<1:
+        return    
+    
+    '''
+    sqlite3.complete_statement(sql) returns True if the string sql contains 
+    one or more complete SQL statements terminated by semicolons. 
+    It does not verify that the SQL is syntactically correct, only that there are 
+    no unclosed string literals and the statement is terminated by a semicolon.
+    This can be used to build a shell for SQLite.
+    '''
+    print("Creating all tables and loading data on the database...")
+    if sqlite3.complete_statement(sqldump):
+        conn = sqlite3.connect(db_filename)
+        cursor = conn.cursor()
+        cursor.executescript(sqldump)
+        conn.close()
+        
+    print("Database created successfully.\n\n")
+    return
+
+
+
+session_create_database()
--- odl_ca_core/sql/sql_server.py
+++ odl_ca_core/sql/sql_server.py
@@ -3,7 +3,7 @@ Created on Jun 22, 2015
 
 @author: fabiomignini
 '''
-import sqlalchemy, sqlite3, os
+import sqlalchemy, os
 from sqlalchemy.orm import sessionmaker
 from odl_ca_core.config import Configuration
 
@@ -48,47 +48,5 @@ def __check_sqlite_database(sqlserverconnection):
     filename = sqlserverconnection[10:]
     return os.path.exists(filename)
 
-
-
-
-
-
-# Work in progress
-def __session_create_database():
-
-    # Relative file addresses
-    db_filename = "../db.sqlite3"
-    dbdumpfile = "../db_dump.sqlite.sql"
-
-    if os.path.exists(db_filename):
-        return
-    if os.path.exists(dbdumpfile) == False:
-        return
-    
-    # Set write permissions on the database file
-    new_db_filename = open(db_filename,'w')
-    os.chmod(db_filename, 0o666)
-    new_db_filename.close()
-    
-    # Read the dump file
-    in_file = open(dbdumpfile,"r")
-    sqldump = in_file.read()
-    if len(sqldump)<1:
-        return    
-    
-    '''
-    sqlite3.complete_statement(sql) returns True if the string sql contains 
-    one or more complete SQL statements terminated by semicolons. 
-    It does not verify that the SQL is syntactically correct, only that there are 
-    no unclosed string literals and the statement is terminated by a semicolon.
-    This can be used to build a shell for SQLite.
-    '''
-    if sqlite3.complete_statement(sqldump):
-        conn = sqlite3.connect(db_filename)
-        cursor = conn.cursor()
-        cursor.executescript(sqldump)
-        conn.close()
-        
-    return
     
     
