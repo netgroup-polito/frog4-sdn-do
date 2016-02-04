6b7797876c15d7840c4b2cf8502579354a279fc4
giacomoratta <giacomoweb@gmail.com>
2016-02-04 15:17:29 +0100

    trunk-vlans [resource description]

--- do_core/resource_description.py
+++ do_core/resource_description.py
@@ -38,6 +38,39 @@ class ResourceDescription(object):  # Singleton Class
         self.__readEndpointsAndVlans()
     
     
+    
+    def updateAll(self):
+        for endpoint_name in self.__endpoints:
+            
+            query = GraphSession().getVlanInIDs(self.__endpoints[endpoint_name]['port'], 
+                                                self.__endpoints[endpoint_name]['switch'])
+            
+            self.__endpoints[endpoint_name]['config']['enabled'] = True
+            busy_vlan_ids = []
+            
+            for q in query:
+                
+                # If the endpoint has an ingress vlan, add the vlan id to the 'trunk-vlan' array
+                if q.vlan_in is not None:
+                    busy_vlan_ids.append(int(q.vlan_in))
+                    
+                # If the endpoint has not an ingress vlan, disable the endpoint
+                else:
+                    self.__endpoints[endpoint_name]['config']['enabled'] = False
+            ''' 
+                ep['trunk_vlans_pointer'] has the reference to list object "trunk-vlans" inside self.__dict.
+                Every modification on ep['trunk_vlans_pointer'] will affect the list object "trunk-vlans".
+            '''
+            new_free_vlan_ids = self.__filter_free_vlan_ids(busy_vlan_ids)
+            self.__endpoints[endpoint_name]['free_vlans'].clear()
+            self.__endpoints[endpoint_name]['free_vlans'].extend(new_free_vlan_ids)
+            
+            # Update trunk-vlan list
+            self.__set_trunk_vlan_list(self.__endpoints[endpoint_name]['trunk_vlans_pointer'],self.__endpoints[endpoint_name]['free_vlans'])
+            
+    
+    
+    
     def saveFile(self):
         '''
         To print a json file with the original order of keys,
@@ -65,35 +98,80 @@ class ResourceDescription(object):  # Singleton Class
             ep['switch'] = name_split[0]
             ep['port'] = name_split[1]
             ep['free_vlans'] = []
+            ep['trunk_vlans_pointer'] = None #reference
             ep['config'] = interface["config"] #reference
             
             interface_vlan = interface["openconfig-if-ethernet:ethernet"]["openconfig-vlan:vlan"]["openconfig-vlan:config"]
             if interface_vlan["interface-mode"] == "TRUNK":
                 if "trunk-vlans" not in interface_vlan:
                     interface_vlan["trunk-vlans"] = []
-                ep['free_vlans'] = interface_vlan["trunk-vlans"] #reference
+                ep['trunk_vlans_pointer'] = interface_vlan["trunk-vlans"] #reference
                 ''' 
-                    ep['free_vlans'] has the reference to list object "trunk-vlans" inside self.__dict.
-                    Every modification on ep['free_vlans'] will affect the list object "trunk-vlans".
+                    ep['trunk_vlans_pointer'] has the reference to list object "trunk-vlans" inside self.__dict.
+                    Every modification on ep['trunk_vlans_pointer'] will affect the list object "trunk-vlans".
                 '''
-                if len(ep['free_vlans'])==0:
+                if len(ep['trunk_vlans_pointer'])==0:
                     update_needed=True
+                    ep['trunk_vlans_pointer'].clear()
                     ep['free_vlans'].clear()
                     ep['free_vlans'].extend(self.__config_vlan_available_ids)
+                    self.__set_trunk_vlan_list(ep['trunk_vlans_pointer'],ep['free_vlans'])
                 
-                # Create an array of vlan id ranges
-                #ep['free_vlans'].clear()
-                #ep['free_vlans'].append = self.__set_available_vlan_ids_array(ep['free_vlans'])
-                
+                else:
+                    ep['free_vlans'].extend(self.__set_available_vlan_ids_array(ep['trunk_vlans_pointer']))
+            
             self.__endpoints[interface['name']] = ep
         
         # Works only at first use/start of the DO
         if update_needed==True:
             self.saveFile()
-
-
-
+    
+    
+    
     def __set_available_vlan_ids_array(self, vid_ranges):
+        
+        '''
+        Expected vid_ranges = "280..289,90..95,18,31,290..299,52,67..82"
+        '''
+        
+        def __getKey(item):
+            return item[0]
+        
+        vid_array = []
+        if isinstance(vid_ranges, str):
+            ranges = vid_ranges.split(",")
+        else:
+            ranges = vid_ranges
+            
+        for r in ranges:
+            r = str(r)
+            exs = r.split("..")
+            if len(exs)==1:
+                exs.append(exs[0])
+            elif len(exs)!=2:
+                continue
+            
+            min_vlan_id = int(exs[0])
+            max_vlan_id = int(exs[1])
+            if (min_vlan_id > max_vlan_id):
+                continue
+            
+            vid_array.append([min_vlan_id,max_vlan_id])
+            logging.debug("[CONFIG] - Available VLAN ID - Range: '"+r+"'")
+        
+        if len(vid_array)==0:
+            logging.error("[CONFIG] - VLAN ID - No available vlan id read from '"+vid_ranges+"'")
+            return []
+        else:
+            return sorted(vid_array,key=__getKey)
+
+
+
+    def __set_available_vlan_ids_array2(self, vid_ranges):
+        
+        '''
+        Expected vid_ranges = "280-289;90-95;290-299;13-56;57-82;2-5;"
+        '''
         
         def __getKey(item):
             return item[0]
@@ -151,6 +229,16 @@ class ResourceDescription(object):  # Singleton Class
         return free_ids
     
     
+    def __set_trunk_vlan_list(self, trunk_vlans_list, free_vlans_list):
+        
+        trunk_vlans_list.clear()
+        
+        for fvid in free_vlans_list:
+            if fvid[0]==fvid[1]:
+                trunk_vlans_list.append(int(fvid[0]))
+            else:
+                trunk_vlans_list.append(str(fvid[0])+".."+str(fvid[1]))
+    
     
     def __set_available_vlan_ids_strings(self, free_ids):
         
@@ -165,32 +253,7 @@ class ResourceDescription(object):  # Singleton Class
         
     
     
-    def updateAll(self):
-        for endpoint_name in self.__endpoints:
-            
-            query = GraphSession().getVlanInIDs(self.__endpoints[endpoint_name]['port'], 
-                                                self.__endpoints[endpoint_name]['switch'])
-            
-            self.__endpoints[endpoint_name]['config']['enabled'] = True
-            busy_vlan_ids = []
-            
-            for q in query:
-                
-                # If the endpoint has an ingress vlan, add the vlan id to the 'trunk-vlan' array
-                if q.vlan_in is not None:
-                    #self.__endpoints[endpoint_name]['free_vlans'].append(int(q.vlan_in))
-                    busy_vlan_ids.append(int(q.vlan_in))
-                    
-                # If the endpoint has not an ingress vlan, disable the endpoint
-                else:
-                    self.__endpoints[endpoint_name]['config']['enabled'] = False
-            ''' 
-                ep['free_vlans'] has the reference to list object "trunk-vlans" inside self.__dict.
-                Every modification on ep['free_vlans'] will affect the list object "trunk-vlans".
-            '''
-            new_free_vlan_ids = self.__filter_free_vlan_ids(busy_vlan_ids)
-            self.__endpoints[endpoint_name]['free_vlans'].clear()
-            self.__endpoints[endpoint_name]['free_vlans'].extend(new_free_vlan_ids)
+    
     
     
     def checkEndpoint(self, switch, port):
