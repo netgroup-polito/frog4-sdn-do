6dd81161e9a71787c908d6aca23e8c2d68248aff
giacomoratta <giacomoweb@gmail.com>
2016-02-05 15:43:05 +0100

    gunicorn on https

--- keys/certificate/certificate.sh
+++ keys/certificate/certificate.sh
@@ -10,6 +10,11 @@ openssl genrsa -des3 -out server.key 2048
 
 openssl rsa -in server.key -out server.key.insecure
 
+# The following two rows create the insecure key, the one without a passphrase, and shuffle the key names.
+# Note: the browser could block this unsafe connection; an explicit authorization is needed. 
+mv server.key server.key.secure
+mv server.key.insecure server.key
+
 openssl req -new -key server.key -out server.csr
 
 openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
