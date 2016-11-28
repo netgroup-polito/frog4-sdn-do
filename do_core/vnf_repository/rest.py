1d1cafa0b3221374b5f1edb2a09dee38ae7c74d8
pamaglio91 <pamaglio@gmail.com>
2016-11-28 12:15:45 +0100

    Added 2 algorithms in case the graph received isn't tagged with the domain field: 1) Added algorithm in that generates a dictionary with the function capability as key and the list of the feasible domain as value. 2)Added algorithm that checks if endpoints' domain matches with the feasible domain from the dictionary

---
+++ do_core/vnf_repository/rest.py
@@ -0,0 +1,47 @@
+"""
+Created on 28/nov/2016
+
+@author: paolomagliona
+"""
+
+
+import requests
+import logging
+import json
+from do_core.controller_interface.rest import RestInterface
+
+
+class VNF_Repository_Rest(RestInterface):
+    version = ""
+
+    def __init__(self, version):
+        pass
+
+    def __logging_debug(self, response, url, jsonFlow=None):
+        log_string = "response: " + str(response.status_code) + ", " + response.reason
+        log_string = url + "\n" + log_string
+        if jsonFlow is not None:
+            log_string = log_string + "\n" + jsonFlow
+        logging.debug(log_string)
+
+    def get_vnf(self, vnf_repository_endpoint, vnf_name):
+        """
+        Return the information of a specific vnf if any
+        :param onos_endpoint: controller REST API address
+        :param onos_user: controller user
+        :param onos_pass: controller password for user
+        :param app_name: the application name
+        :return:
+        """
+        headers = {'Accept': 'application/json'}
+        url = vnf_repository_endpoint + self.vnf_repository_url + "/" + str(vnf_name)
+
+        #response = requests.get(url, headers=headers, auth=(vnf_repository_user, vnf_repository_pass))
+        response = False
+
+        #self.__logging_debug(response, url)
+        #response.raise_for_status()
+        #return response.text
+        return response
+
+
